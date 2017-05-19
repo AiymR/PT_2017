e00622ed78abd4d9f354d51862e6d92959fd1c19
AiymR <raikhanovaaiym@gmail.com>
2017-05-19 14:45:51 +0600

    Add files via upload

---
+++ ClockAnalogue/DigitalClock/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.T_tick);
+            //start timer when forms loads
+            t.Start();
+        }
+        private void T_tick(object sender, EventArgs e)
+
+        {
+            //get current time
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss= DateTime.Now.Second;
+
+            //time
+            string time = "";
+
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            } else
+            {
+                time += hh;
+            }
+            time += ":";
+
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+            {
+                time += mm;
+            }
+            time += ":";
+            if (ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+            {
+                time += ss;
+            }
+            time += "";
+            label1.Text = time;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
